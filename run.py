2335680b9cc815f7c21e8a44cc87d2db1dc0c76b
Septian Hari <septian@infoflow.co.id>
2019-01-17 17:21:26 +0700

    Loading image intro

--- run.py
+++ run.py
@@ -4,7 +4,21 @@ pixel_unit = 36
 n_tiles = 5
 screen = pygame.display.set_mode((pixel_unit * n_tiles, pixel_unit * n_tiles))
 
-done = False
-while not done:
-  for event in pygame.event.get():
-    print(event)
+circle_black = pygame.image.load("images/circle_black.png")
+circle_white = pygame.image.load("images/circle_white.png")
+tile = pygame.image.load("images/tile.png")
+
+circle_black_pos = (0,0)
+circle_white_pos = (pixel_unit * (n_tiles - 1), pixel_unit * (n_tiles - 1))
+tile_array_pos = []
+for i in range(0, n_tiles):
+  for j in range(0, n_tiles):
+    tile_array_pos.append((i * pixel_unit, j * pixel_unit))
+
+screen.fill((255,255,255))
+for position in tile_array_pos:
+  screen.blit(tile, position)
+screen.blit(circle_black, circle_black_pos)
+screen.blit(circle_white, circle_white_pos)
+# rendering things with flip
+pygame.display.flip()
