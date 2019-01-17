e3262bb0f1461fedd2ecea93e77a062dc1cb1446
Septian Hari <septian@infoflow.co.id>
2019-01-17 17:07:36 +0700

    Event intro

--- run.py
+++ run.py
@@ -4,4 +4,7 @@ pixel_unit = 36
 n_tiles = 5
 screen = pygame.display.set_mode((pixel_unit * n_tiles, pixel_unit * n_tiles))
 
-
+done = False
+while not done:
+  for event in pygame.event.get():
+    print(event)
